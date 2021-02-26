2d192cbcea29f13e996dc363e5cc08ecf137cc64
pppedro173 <34283375+pppedro173@users.noreply.github.com>
2021-02-26 23:23:56 +0000

    Add files via upload

--- test.c
+++ test.c
@@ -3,57 +3,34 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 char *const unalterable_pointer = "abcdefghijklmnopqrstuvwxyz";
 
-void magic(char *veccar, int *index, int *counter, char car, int size, int *index2, char *auxalphabet)
+void magic(char *veccar, int *index, long *counter, char car, int size, int *index2, char *auxalphabet)
 {
     int i=0;
-    if (*counter == 26)
-        return;
-    if (*index == size)
-        return;
-    if (veccar[*index + 1] != car)
-        magic(veccar, index, counter, unalterable_pointer[ *index2 = *index2 + 1], size, index2,auxalphabet);
-    if (veccar[*index + 1] == car )
+    if (*counter == 26) return;
+    if (*index == size) return;
+    if (veccar[*index + 1] != car) magic(veccar, index, counter, unalterable_pointer[*index2 = *index2 + 1], size, index2,auxalphabet);
+    if (veccar[*index + 1] == car)
     {
-        for(i=0;i<26;i++) {
-            if (auxalphabet[i] == car) {
-                *counter = *counter-1;
-                break;
-                }
-            };
-        auxalphabet[*counter+1] = car;
-        *counter = *counter + 1;
-        *index2 = 0;
-        *index = *index + 1;
+        for(i=0;i<26;i++) {if (auxalphabet[i] == car) {*counter = *counter-1; break;}};
+        auxalphabet[*counter+1] = car; *counter = *counter + 1; *index2 = 0; *index = *index + 1;
         magic(veccar, index, counter, unalterable_pointer[*index2], size, index2,auxalphabet);
     }
 }
 
 void main(int argc, char *argv[])
 {
-    int counter = 0;
-    int size = 0;
-    char *string;
-    int i = 0;
-    int index = 0;
-    int index2 = 0;
-    char *auxalphabet =calloc(27,sizeof(char));
-    if (argc > 2 || argc < 2)
-        return;
+    long counter = 0; int size = 0; char *string; int i = 0; int index = 0; int index2 = 0; char *auxalphabet =calloc(27,sizeof(char));
+    if (argc > 2 || argc < 2) return;
     string = (char *)malloc((strlen(argv[1]) + 1) * sizeof(char));
     strcpy(string, argv[1]);
     size = strlen(string);
-    for (; *string; ++string)
-        *string = tolower(*string);
-    for (i = 0; i < size + 1; i++)
-        --string;
+    for (; *string; ++string) *string = tolower(*string);
+    for (i = 0; i < size + 1; i++) --string;
     magic(string, &index, &counter, unalterable_pointer[index2], size, &index2,auxalphabet);
-    if (counter == 26)
-    {
-        printf("*************TRUE**********************");
-        return;
-    };
+    if (counter == 26){printf("*************TRUE**********************"); return;};
     printf("*************False**********************");
 }
